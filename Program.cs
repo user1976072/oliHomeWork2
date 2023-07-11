338c856e1fb4b6df372705d559d1b97d48560cfc
Olga <user1976072@gmail.com>
2023-07-11 12:58:11 +0300

    рeшение ДЗ к 2 семинару

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿void SecondNum(int number)
+{
+    int SecondNum = (number / 10) % 10;
+    Concole.WriteLine ("Вторая цифра числа" + SecondNum);
+} 
+
+Concole.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Concole.RedLain());
+
