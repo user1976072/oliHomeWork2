272391cb4aae5e0ccbfa6e38ee5139da4c29d72d
Olga <user1976072@gmail.com>
2023-07-13 17:38:38 +0300

    рeшение ДЗ к 2 семинару 3 задания

--- Program.cs
+++ Program.cs
@@ -1,9 +1,63 @@
-﻿void SecondNum(int number)
+﻿// решение заданий к семинару №2
+
+//Задача 10
+/*
+
+void SecondNum(int number)
 {
     int SecondNum = (number / 10) % 10;
-    Concole.WriteLine ("Вторая цифра числа" + SecondNum);
+    Console.WriteLine ("Вторая цифра числа: " + SecondNum);
 } 
 
-Concole.WriteLine("Enter your number: ");
-int number = Convert.ToInt32(Concole.RedLain());
+Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SecondNum (number);
+*/
+
+// Задача №13
+/*
+void ThirdNum(int number)
+{
+  if (number > 99 && number <= 999)
+  {
+    number = number % 10;
+    Console.WriteLine("Третье число: " + number);
+  }
+  else if (number > 999 && number <= 9999)
+    {
+         number = (number / 10) % 10;
+         Console.WriteLine("Третье число: " + number);
+    }
+       else if (number > 9999 && number <= 99999)
+       {
+              number = (number / 100) % 10;
+              Console.WriteLine("Третье число: " + number);
+       }
+            else if (number < 100)
+                   Console.WriteLine("Третья цифра числа отсутствует");
+                 else 
+                   Console.WriteLine("Число имеет более 5 цифр");
+}
+Console.WriteLine("Укажите число: ");
+int Tnumber = Convert.ToInt32(Console.ReadLine());
+ThirdNum(Tnumber);
+*/
+
+
+// Задача № 15
+
+void Week(int Day)
+{
+  if (Day == 6) 
+    Console.WriteLine ($"является выходным днем {Day}");
+  else if (Day == 7) 
+    Console.WriteLine ($"является выходным днем {Day}");
+       else
+         Console.WriteLine ($"не является выходным днем {Day}");
+}
+
+Console.WriteLine ("Укажи день недели: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+Week(userNum);
 
