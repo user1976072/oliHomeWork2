285bcd0c7490aacda7c54c6b577230dc64a70672
Olga <user1976072@gmail.com>
2023-07-25 21:48:47 +0300

    рeшение ДЗ к 6 семинару 2 задания

--- Program.cs
+++ Program.cs
@@ -1,4 +1,67 @@
-﻿// Решение заданий к семинару №5
+﻿// Решение заданий к семинару №6
+// Задача 41: 
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл 
+//пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+/*
+void Massiv(int count)
+{
+    int[] array = new int[count];
+    int countPos = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+            countPos++;
+        Console.WriteLine($"Количество чисел больше 0: {countPos}");
+    }
+}
+Console.Write("Введите количество чисел, которое хотите ввести: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Massiv(M);
+*/
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+//задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// 1 способ без массива
+
+void Metod()
+{
+    Console.Write("Введите значение k1: ");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите значение b1: ");
+    double b1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите значение k2: ");
+    double k2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите значение b2: ");
+    double b2 = Convert.ToInt32(Console.ReadLine());
+
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Точка пересечения: ({x}, {y})");
+}
+Metod();
+
+//2 способ с массивом
+/*
+void Method(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.Write($"Координаты точки пересечения двух линий: {x}, {y}");
+}
+
+Method();
+
+*/
+// Решение заданий к семинару №5
 // Задача 34: 
 // Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
@@ -95,7 +158,7 @@ CountArray(randomArray);
 // Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
-
+/*
 double[] Massiv(int length)
 {
     double[] array = new double[length];
@@ -142,7 +205,7 @@ void RazArray(double[] array)
 double[] randomArray = Massiv(5);
 PrintArray(randomArray);
 RazArray(randomArray);
-
+*/
 // Console.WriteLine($" Max: {maxElem:f2}");
 // Console.WriteLine($" Min: {minElem:f2}");
 // Решение заданий к семинару №4
